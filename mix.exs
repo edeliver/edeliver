e3bf9215b91b373db46d4f18a6b42dbfb4c3a705
Bertram Harendt <bertram@sauspiel.de>
2015-05-22 14:31:12 +0200

    Add hex metadata.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,20 @@ defmodule Edeliver.Mixfile do
 
   def project do
     [
-      version: "1.0.0"
+      version: "1.0.0",
+      description:  "Build and Deploy Elixir Applications and perform Hot-Code Upgrades",
+      package: [
+        licenses: ["MIT"],
+        files:  [
+          "bin",
+          "lib",
+          "libexec",
+          "src",
+          "strategies",
+          "README.md",
+        ],
+        links: %{"GitHub" => "https://github.com/boldpoker/edeliver"},
+      ],
     ]
   end
 end
