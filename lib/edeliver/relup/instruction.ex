5730ccb1112b11dfac01fc5fa98b08126d6517e2
Bertram Harendt <bertram@sauspiel.de>
2016-01-19 15:29:41 +0100

    Ensure that the code of any `RunnableInstruction` is loaded before executing that instruction
    during the upgrade.

--- lib/edeliver/relup/instruction.ex
+++ lib/edeliver/relup/instruction.ex
@@ -47,28 +47,140 @@ defmodule Edeliver.Relup.Instruction do
         failing instructions running after that point will cause the node to restart the release.
       """
       @spec insert_before_point_of_no_return(%Instructions{}|instructions, new_instructions::instruction|instructions) :: updated_instructions::%Instructions{}|instructions
-      def insert_before_point_of_no_return(instructions = %Instructions{up_instructions: up_instructions, down_instructions: down_instructions}, new_instructions) do
+      def insert_before_point_of_no_return(instructions = %Instructions{}, new_instructions) do
         %{instructions|
           up_instructions:   insert_before_point_of_no_return(instructions.up_instructions,   new_instructions),
           down_instructions: insert_before_point_of_no_return(instructions.down_instructions, new_instructions)
         }
       end
-      def insert_before_point_of_no_return(existing_instructions, new_instruction) when is_list(existing_instructions) and not is_list(new_instruction) do
-        insert_before_point_of_no_return(existing_instructions, [new_instruction])
-      end
-      def insert_before_point_of_no_return(existing_instructions, new_instructions) when is_list(existing_instructions) do
-        insert_before_point_of_no_return(existing_instructions, new_instructions, [])
+      def insert_before_point_of_no_return(existing_instructions, new_instructions) do
+        insert_before_instruction(existing_instructions, new_instructions, :point_of_no_return)
       end
 
-      defp insert_before_point_of_no_return(existing_instructions = [:point_of_no_return|_], new_instructions, instructions_before_point_of_no_return) do
-        Enum.reverse(instructions_before_point_of_no_return) ++ new_instructions ++ existing_instructions
+
+      @doc """
+        Inserts an instruction or a list of instructions before the given instruction.
+      """
+      @spec insert_before_instruction(%Instructions{}|instructions, new_instructions::instruction|instructions, before_instruction::instruction) :: updated_instructions::%Instructions{}|instructions
+      def insert_before_instruction(instructions = %Instructions{}, new_instructions, before_instruction) do
+        %{instructions|
+          up_instructions:   insert_before_instruction(instructions.up_instructions,  new_instructions, before_instruction),
+          down_instructions: insert_after_instruction(instructions.down_instructions, new_instructions, before_instruction)
+        }
       end
-      defp insert_before_point_of_no_return(_existing_instructions = [no_point_of_no_return_instruction|rest], new_instructions, instructions_before_point_of_no_return) do
-        insert_before_point_of_no_return(rest, new_instructions, [no_point_of_no_return_instruction|instructions_before_point_of_no_return])
+      def insert_before_instruction(existing_instructions, new_instruction, before_instruction) when is_list(existing_instructions) and not is_list(new_instruction) do
+        insert_before_instruction(existing_instructions, [new_instruction], before_instruction)
       end
-      defp insert_before_point_of_no_return(_existing_instructions = [], new_instructions, instructions_before_point_of_no_return) do
-        Enum.reverse(instructions_before_point_of_no_return) ++ new_instructions
+      def insert_before_instruction(existing_instructions, new_instructions, before_instruction) when is_list(existing_instructions) do
+        insert_before_instruction(existing_instructions, new_instructions, before_instruction, [])
       end
+
+      defp insert_before_instruction(existing_instructions = [before_instruction|_], new_instructions, before_instruction, instructions_before_instruction) do
+        Enum.reverse(instructions_before_instruction) ++ new_instructions ++ existing_instructions
+      end
+      defp insert_before_instruction(_existing_instructions = [no_point_of_no_return_instruction|rest], new_instructions, before_instruction, instructions_before_instruction) do
+        insert_before_instruction(rest, new_instructions, before_instruction, [no_point_of_no_return_instruction|instructions_before_instruction])
+      end
+      defp insert_before_instruction(_existing_instructions = [], new_instructions, _before_instruction, instructions_before_instruction) do
+        Enum.reverse(instructions_before_instruction) ++ new_instructions
+      end
+
+
+      @doc """
+        Inserts an instruction or a list of instructions after the given instruction.
+      """
+      @spec insert_after_instruction(%Instructions{}|instructions, new_instructions::instruction|instructions, after_instruction::instruction) :: updated_instructions::%Instructions{}|instructions
+      def insert_after_instruction(instructions = %Instructions{}, new_instructions, after_instruction) do
+        %{instructions|
+          up_instructions:   insert_after_instruction(instructions.up_instructions,  new_instructions, after_instruction),
+          down_instructions: insert_before_instruction(instructions.down_instructions, new_instructions, after_instruction)
+        }
+      end
+      def insert_after_instruction(existing_instructions, new_instruction, after_instruction) when is_list(existing_instructions) and not is_list(new_instruction) do
+        insert_after_instruction(existing_instructions, [new_instruction], after_instruction)
+      end
+      def insert_after_instruction(existing_instructions, new_instructions, after_instruction) when is_list(existing_instructions) do
+        insert_after_instruction(existing_instructions, new_instructions, after_instruction, [])
+      end
+
+      defp insert_after_instruction(_existing_instructions = [after_instruction|rest], new_instructions, after_instruction, instructions_before_instruction) do
+        Enum.reverse(instructions_before_instruction) ++ [after_instruction|new_instructions] ++ rest
+      end
+      defp insert_after_instruction(_existing_instructions = [no_point_of_no_return_instruction|rest], new_instructions, after_instruction, instructions_before_instruction) do
+        insert_after_instruction(rest, new_instructions, after_instruction, [no_point_of_no_return_instruction|instructions_before_instruction])
+      end
+      defp insert_after_instruction(_existing_instructions = [], new_instructions, _after_instruction, instructions_before_instruction) do
+        Enum.reverse(instructions_before_instruction) ++ new_instructions
+      end
+
+      @doc """
+        Ensures that the given module is loaded before the given instruction (if it needs to be loaded).
+        If an `%Instructions{}` is given containing also the down instructions, it ensures that the module
+        is unloaded after the instruction for the down instructions.
+      """
+      @spec ensure_module_loaded_before_instruction(%Instructions{}|instructions, instruction::instruction, module::atom) :: updated_instructions::%Instructions{}|instructions
+      def ensure_module_loaded_before_instruction(instructions = %Instructions{}, instruction, module) do
+        %{instructions|
+          up_instructions:   ensure_module_loaded_before_instruction(instructions.up_instructions, instruction, module),
+          down_instructions: ensure_module_unloaded_after_instruction(instructions.down_instructions, instruction, module)
+        }
+      end
+      def ensure_module_loaded_before_instruction(up_instructions, instruction, module) when is_list(up_instructions) do
+        ensure_module_loaded_before_instruction(up_instructions, instruction, module, [])
+      end
+      def ensure_module_loaded_before_instruction(instructions, instruction), do: ensure_module_loaded_before_instruction(instructions, instruction, __MODULE__)
+
+      defp ensure_module_loaded_before_instruction(_instructions = [cur_instruction|rest], instruction, module, checked_instructions) do
+        case cur_instruction do
+          {:load_module, ^module} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load_module, ^module, _dep_mods} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load_module, ^module, _pre_purge, _post_purge, _dep_mods} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:add_module,  ^module} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:add_module,  ^module, _dep_mods} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load,       {^module, _pre_purge, _post_purge}} -> insert_before_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          _ -> ensure_module_loaded_before_instruction(rest, instruction, module, [cur_instruction|checked_instructions])
+        end
+      end
+      defp ensure_module_loaded_before_instruction(_instructions = [], _instruction, _module, checked_instructions) do
+        Enum.reverse(checked_instructions)
+      end
+
+      @doc """
+        Ensures that the given module is (un)loaded after the given instruction (if it needs to be (un)loaded).
+        If an `%Instructions{}` is given containing also the down instructions, it ensures that the module
+        is (un)loaded before the instruction for the down instructions.
+      """
+      @spec ensure_module_unloaded_after_instruction(%Instructions{}|instructions, instruction::instruction, module::atom) :: updated_instructions::%Instructions{}|instructions
+      def ensure_module_unloaded_after_instruction(instructions = %Instructions{}, instruction, module) do
+        %{instructions|
+          up_instructions:   ensure_module_unloaded_after_instruction(instructions.up_instructions, instruction, module),
+          down_instructions: ensure_module_loaded_before_instruction(instructions.down_instructions, instruction, module)
+        }
+      end
+      def ensure_module_unloaded_after_instruction(up_instructions, instruction, module) when is_list(up_instructions) do
+        ensure_module_unloaded_after_instruction(up_instructions, instruction, module, [])
+      end
+      def ensure_module_unloaded_after_instruction(instructions, instruction), do: ensure_module_unloaded_after_instruction(instructions, instruction, __MODULE__)
+
+      defp ensure_module_unloaded_after_instruction(_instructions = [cur_instruction|rest], instruction, module, checked_instructions) do
+        case cur_instruction do
+          {:load_module, ^module} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load_module, ^module, _dep_mods} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load_module, ^module, _pre_purge, _post_purge, _dep_mods} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:add_module,  ^module} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:add_module,  ^module, _dep_mods} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:load,       {^module, _pre_purge, _post_purge}} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:purge, [^module]} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:remove, {^module, _pre_purge, _post_purge}} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:delete_module, ^module} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          {:delete_module, ^module, _dep_mods} -> insert_after_instruction(Enum.reverse(checked_instructions) ++ rest, cur_instruction, instruction)
+          _ -> ensure_module_unloaded_after_instruction(rest, instruction, module, [cur_instruction|checked_instructions])
+        end
+      end
+      defp ensure_module_unloaded_after_instruction(_instructions = [], _instruction, _module, checked_instructions) do
+        Enum.reverse(checked_instructions)
+      end
+
     end
   end
 
