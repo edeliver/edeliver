00a76ae6d7ce8684639c17dca4926dd276c46dfe
Bertram Harendt <bertram@sauspiel.de>
2016-01-19 16:38:50 +0100

    Check whether any process runs old code before upgrading release.

--- lib/edeliver/relup/instructions/check_processes_running_old_code.ex
+++ lib/edeliver/relup/instructions/check_processes_running_old_code.ex
@@ -5,6 +5,15 @@ defmodule Edeliver.Relup.Instructions.CheckProcessesRunningOldCode do
 
   def run(_arguments) do
     Logger.info "Validating no process runs old code before upgrading..."
+    modules = :erlang.loaded
+    modules_running_old_code = Enum.filter(modules, fn(module) ->
+      :erlang.check_old_code(module)
+    end)
+    if modules_running_old_code == [] do
+      Logger.info "None of #{inspect Enum.count(modules)} runs old code."
+    else
+      throw {:error, {:running_old_code, modules_running_old_code}}
+    end
   end
 
 end
