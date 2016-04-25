b106ce268b03fc5045a933aa34e08e0e8a518cb9
Bertram Harendt <bertram@sauspiel.de>
2016-04-25 18:30:21 +0200

    Improve displaying of revisions contained into the running release.

---
+++ test/common_functions_test.exs
@@ -0,0 +1,41 @@
+
+defmodule Edeliver.CommonFunctions.Test do
+  use Edeliver.BashScript.Case, bash_script: Path.join([__DIR__, "..", "libexec", "erlang"])
+
+  test "trimming strings" do
+    assert "foo" = call "trim_string", " foo"
+    assert "bar" = call "trim_string", "bar "
+    assert "baz" = call "trim_string", " baz "
+    assert "don't baz" = call "trim_string", " don't baz "
+  end
+
+  test "joining string" do
+    assert "foo bar" = call "__join_string", " ", "foo", "bar"
+    assert "foo bar" = call "__join_string", " ", " foo", "bar "
+    assert "foo,bar" = call "__join_string", ",", "foo", "bar"
+  end
+
+  test "get args for release.version task" do
+    assert "" = call "__get_auto_version_args"
+    with_env INCREMENT_RELEASE_VERSION: "major" do
+      assert "major" = call "__get_auto_version_args"
+    end
+    with_env SET_RELEASE_VERSION: "1.2.3-beta.1" do
+      assert "1.2.3-beta.1" = call "__get_auto_version_args"
+    end
+    with_env AUTO_RELEASE_VERSION: "revison+branch" do
+      assert "revison+branch" = call "__get_auto_version_args"
+    end
+    with_env INCREMENT_RELEASE_VERSION: "minor", AUTO_RELEASE_VERSION: "revison+branch" do
+      assert "minor revison+branch" = call "__get_auto_version_args"
+    end
+    with_env SET_RELEASE_VERSION: "1.2.3-beta.1", AUTO_RELEASE_VERSION: "commit-count" do
+      assert "1.2.3-beta.1 commit-count" = call "__get_auto_version_args"
+    end
+    with_env INCREMENT_RELEASE_VERSION: " patch ", AUTO_RELEASE_VERSION: " revision " do
+      assert "patch revision" = call "__get_auto_version_args"
+    end
+  end
+
+
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,56 @@
 
 
+defmodule Edeliver.BashScript.Case do
+  use ExUnit.CaseTemplate
+
+  using(args) do
+    quote do
+      @bash_script_file Keyword.get(unquote(args), :bash_script)
+
+      @doc "Calls a function in the bash script"
+      @spec call(bash_script_file::Path.t, function::String.t, args::[String.t]) :: String.t
+      def call(bash_script_file = <<_,_::binary>>, function = <<_,_::binary>>, args) when is_list(args) do
+        assert File.exists? bash_script_file
+        args = args |> Enum.map(&("\\\"#{&1}\\\"")) |> Enum.join(" ")
+        "/usr/bin/env bash <<<\"
+          set -e
+          source \\\"#{bash_script_file}\\\"
+          2>&1 #{function} #{args}
+        \"" |> String.to_char_list() |> :os.cmd() |> IO.chardata_to_string() |> String.rstrip(?\n)
+      end
+      def call(function = <<_,_::binary>>, arg1 = <<_,_::binary>>, arg2 = <<_,_::binary>>), do: call(function, [arg1, arg2])
+      def call(function = <<_,_::binary>>, arg1 = <<_,_::binary>>), do: call(function, [arg1])
+      def call(function = <<_,_::binary>>, args) when is_list(args), do: call(@bash_script_file, function, args)
+      def call(function = <<_,_::binary>>, arg1 = <<_,_::binary>>, arg2 = <<_,_::binary>>, arg3 = <<_,_::binary>>), do: call(function, [arg1, arg2, arg3])
+      def call(function = <<_,_::binary>>), do: call(function, [])
+      def call(function = <<_,_::binary>>, arg1, arg2, arg3, arg4), do: call(function, [arg1, arg2, arg3, arg4])
+      def call(function = <<_,_::binary>>, arg1, arg2, arg3, arg4, arg5), do: call(function, [arg1, arg2, arg3, arg4, arg5])
+      def call(function = <<_,_::binary>>, arg1, arg2, arg3, arg4, arg5, arg6), do: call(function, [arg1, arg2, arg3, arg4, arg5, arg6])
+
+      @doc "Executes `expression` while setting the given environment variables"
+      defmacro with_env(env_assignments, expression) do
+        quote do
+          old_env = System.get_env
+          for {env, value} <- unquote(env_assignments) do
+            env |> Atom.to_string() |> System.put_env(value)
+          end
+          try do
+            unquote(expression)
+          after
+            for {env, value} <- unquote(env_assignments) do
+              env = Atom.to_string(env)
+              case Map.get(old_env, env) do
+                nil -> System.delete_env(env)
+                value -> System.put_env(env, value)
+              end
+            end
+          end
+        end
+      end
+
+    end # quote
+  end # using
+end #defmodule
+
 
 ExUnit.start()
