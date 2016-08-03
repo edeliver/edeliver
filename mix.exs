9d4c926642789756a03224f2aa2399a3cfad0926
Bertram Harendt <bertram@sauspiel.de>
2016-08-03 19:01:44 +0200

    Use correct semantic version for distillery dependency.

--- mix.exs
+++ mix.exs
@@ -36,13 +36,13 @@ defmodule Edeliver.Mixfile do
   defp deps do
     if project_uses_distillery? do
       [
-        {:distillery, ">= 0.8", warn_missing: false},
+        {:distillery, ">= 0.8.0", warn_missing: false},
         {:exrm, ">= 0.16.0", optional: true, warn_missing: false},
       ]
     else
       [
         {:exrm, ">= 0.16.0", warn_missing: false},
-        {:distillery, ">= 0.8", optional: true, warn_missing: false},
+        {:distillery, ">= 0.8.0", optional: true, warn_missing: false},
       ]
     end ++
     [
