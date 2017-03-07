4c940dac91f6848c8ab4349a0d2cf1c377ce82ea
Ian Goodrich <igoodrich@gmail.com>
2017-03-06 16:05:59 -0800

    Revert "semaphore fix 2"
    This reverts commit f2d1946bfa0eeecf45653663c8d5772b3950f419.

--- mix.exs
+++ mix.exs
@@ -67,7 +67,52 @@ defmodule Edeliver.Mixfile do
   end
 
   defp project_uses_distillery? do
-    true
+    try do
+      deps = Mix.Dep.Loader
+          |> Kernel.apply(:children)
+          |> Enum.map(&(Map.get(&1, :app)))
+      uses_distillery? = Enum.member?(deps, :distillery)
+      uses_exrm? = Enum.member?(deps, :exrm)
+      cond do
+        uses_distillery? and uses_exrm? ->
+          case System.get_env("USING_DISTILLERY") do
+            "false" -> false
+            "true"  -> true
+            _ ->
+              warning "Warning: Detected that both, :distillery and :exrm are used as dependency.\n"
+                   <> "         edeliver will use :distillery as build tool unless you remove it \n"
+                   <> "         as dependency or set the environment variable USING_DISTILLERY='false'."
+              true
+          end
+        uses_distillery? -> true
+        uses_exrm? -> false
+        true ->
+          case System.get_env("PUBLISHING_TO_HEX_PM") do
+            "true" -> false
+            _ ->
+              Mix.Shell.IO.error "Failed to detect whether :distillery or :exrm is used as dependency.\n"
+                              <> "If you used exrm before (default), please add it to your mix.exs\n"
+                              <> "config file like this:\n\n"
+                              <> "defp deps do\n"
+                              <> "  [\n"
+                              <> "   ...\n"
+                              <> "   {:exrm, \">= 0.16.0\", warn_missing: false},\n"
+                              <> "  ]\n"
+                              <> "end\n\n"
+                              <> "or upgrade to distillery as build tool. You find more information\n"
+                              <> "about how to upgrade on the edeliver wiki page:\n\n"
+                              <> "https://github.com/boldpoker/edeliver/wiki/Upgrade-from-exrm-to-distillery-as-build-tool\n\n"
+
+              System.halt(1)
+          end
+      end
+      rescue error ->
+        Mix.Shell.IO.error "Error when detecting whether distillery or exrm is used as release build tool: #{inspect error}"
+        System.halt(1)
+      catch signal, error ->
+        Mix.Shell.IO.error "Failed to detect whether distillery or exrm is used as release build tool with signal: #{inspect signal} and error: #{inspect error}"
+        System.halt(1)
+    end
   end
 
   defp warning(message) do
