03d393333e6ef493523c5c9c879119b7d6399b92
bharendt <bertram@sauspiel.de>
2022-05-19 09:57:48 +0200

    Rename elixir and distillery mix modules

--- test/configs/mix-distillery.exs
+++ test/configs/mix-distillery.exs
@@ -1,4 +1,4 @@
-defmodule Mydist.Mixfile do
+defmodule EcoDistillery.Mixfile do
   use Mix.Project
 
   def project do
--- test/configs/mix-elixir.exs
+++ test/configs/mix-elixir.exs
@@ -1,4 +1,4 @@
-defmodule Mydist.Mixfile do
+defmodule Eco.Mixfile do
   use Mix.Project
 
   def project do
